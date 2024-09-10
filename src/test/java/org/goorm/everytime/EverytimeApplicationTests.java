a9ebd1e40fb7d88d45a5a0dcea42d4db692c0cce
Hyun jinyong <ekwls20@gmail.com>
2024-09-10 14:05:23 +0900

    project creates

---
+++ src/test/java/org/goorm/everytime/EverytimeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goorm.everytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverytimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
